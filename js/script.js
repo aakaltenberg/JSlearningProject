66992066574f3ab857eeea72bb1c74b8b799eab8
aakaltenberg <a.a.kaltenberg@yandex.ru>
2022-04-03 17:26:16 +0300

    first lesson .js | console application

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+// 1)
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+
+// 2)
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+// 3)
+
+let answerFirstHowFilmIsWatched = prompt('Один из просмотренных фильмов?',''),
+    answerFirstFilmsNote = prompt('Какую оценку поставите?',''),
+    answerSecondHowFilmIsWatched = prompt('Один из просмотренных фильмов?',''),
+    answerSecondFilmsNote = prompt('Какую оценку поставите?','');
+
+personalMovieDb.movies[answerFirstHowFilmIsWatched] = answerFirstFilmsNote;
+personalMovieDb.movies[answerSecondHowFilmIsWatched] = answerSecondFilmsNote;
+
+console.log(personalMovieDb);
