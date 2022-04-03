3a74c9e91496a10d6a8e942b075dd09b34f3a27d
aakaltenberg <a.a.kaltenberg@yandex.ru>
2022-04-03 20:58:43 +0300

    .js lesson3 | simple functions

--- js/script.js
+++ js/script.js
@@ -1,44 +1,75 @@
 "use strict";
 
-// 1)
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
 
-// 2)
+let numberOfFilms,
+    definition;
+
+function start(){
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+    }
+}
+
+function rememberMyFilms(){
+    for(let i = 1; i <= personalMovieDb.count; i++){
+
+        let answerHowFilmIsWatched,
+        answerFilmsNote;
+    
+        answerHowFilmIsWatched = prompt(`Как называется фильм №${i}?`,'');
+    
+        if(answerHowFilmIsWatched == null || answerHowFilmIsWatched.length == 0 || answerHowFilmIsWatched.length > 50){
+            i--;
+            continue;
+        }
+    
+        answerFilmsNote = prompt(`Оцените фильм №${i}`,'');
+    
+        personalMovieDb.movies[answerHowFilmIsWatched] = answerFilmsNote;
+    }
+}
+
+function detectPersonalLevel(){
+    if(personalMovieDb.count > 30){
+        definition = 'Вы киноман';
+    } else if (personalMovieDb.count > 10 ){
+        definition = 'Вы классический зритель';
+    }
+    else if(personalMovieDb.count <= 10){
+        definition = 'Просмотрено довольно мало фильмов';
+    }
+    else{
+        definition = 'Ошибка';
+    }
+}
+
+start();
 const personalMovieDb = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    private: false
+    private: false,
+    personalLevel: ''
 };
+rememberMyFilms();
+detectPersonalLevel();
+personalMovieDb.personalLevel = definition;
 
-// 3)
-
-for(let i = 1; i <= personalMovieDb.count; i++){
-
-    let answerHowFilmIsWatched,
-    answerFilmsNote;
-
-    answerHowFilmIsWatched = prompt(`Как называется фильм №${i}?`,'');
-
-    if(answerHowFilmIsWatched == null || answerHowFilmIsWatched.length == 0 || answerHowFilmIsWatched.length > 50){
-        i--;
-        alert('Название не может быть пустым, или иметь длину, превышающую 50 символов')
-        continue;
+function showMyDB(){
+    if(personalMovieDb.private){
+        console.log('Внимание, нет прав для просмотра объекта');
+    } else{
+        console.log(personalMovieDb);
     }
-
-    answerFilmsNote = prompt(`Оцените фильм №${i}`,'');
-
-    personalMovieDb.movies[answerHowFilmIsWatched] = answerFilmsNote;
 }
 
-let definition = '';
-
-personalMovieDb.count > 30 
-    ? definition = 'Вы киноман' 
-    : (personalMovieDb.count > 10 
-        ? definition = 'Вы классический зритель' 
-        : definition = 'Просмотрено довольно мало фильмов');
-
-console.log(personalMovieDb.movies);
-console.log(definition);
+function writeYourGenres(){
+    for(let i = 0; i < 3; i++){
+        personalMovieDb.genres[i] = prompt(`Ваш любимый жанр под номером ${i}?`,'');
+    }
+}
+writeYourGenres();
+showMyDB();
