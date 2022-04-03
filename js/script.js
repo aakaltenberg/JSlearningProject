60d543ca53ef7ade093c5c230f2cef26fe503262
aakaltenberg <a.a.kaltenberg@yandex.ru>
2022-04-03 17:54:47 +0300

    .js course, lesson2 | cicles and conditions

--- js/script.js
+++ js/script.js
@@ -14,12 +14,31 @@ const personalMovieDb = {
 
 // 3)
 
-let answerFirstHowFilmIsWatched = prompt('Один из просмотренных фильмов?',''),
-    answerFirstFilmsNote = prompt('Какую оценку поставите?',''),
-    answerSecondHowFilmIsWatched = prompt('Один из просмотренных фильмов?',''),
-    answerSecondFilmsNote = prompt('Какую оценку поставите?','');
+for(let i = 1; i <= personalMovieDb.count; i++){
 
-personalMovieDb.movies[answerFirstHowFilmIsWatched] = answerFirstFilmsNote;
-personalMovieDb.movies[answerSecondHowFilmIsWatched] = answerSecondFilmsNote;
+    let answerHowFilmIsWatched,
+    answerFilmsNote;
 
-console.log(personalMovieDb);
+    answerHowFilmIsWatched = prompt(`Как называется фильм №${i}?`,'');
+
+    if(answerHowFilmIsWatched == null || answerHowFilmIsWatched.length == 0 || answerHowFilmIsWatched.length > 50){
+        i--;
+        alert('Название не может быть пустым, или иметь длину, превышающую 50 символов')
+        continue;
+    }
+
+    answerFilmsNote = prompt(`Оцените фильм №${i}`,'');
+
+    personalMovieDb.movies[answerHowFilmIsWatched] = answerFilmsNote;
+}
+
+let definition = '';
+
+personalMovieDb.count > 30 
+    ? definition = 'Вы киноман' 
+    : (personalMovieDb.count > 10 
+        ? definition = 'Вы классический зритель' 
+        : definition = 'Просмотрено довольно мало фильмов');
+
+console.log(personalMovieDb.movies);
+console.log(definition);
