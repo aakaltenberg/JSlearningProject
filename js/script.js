e255cffcce697a077cd3e9e69c8e0ed1a771211a
aakaltenberg <a.a.kaltenberg@yandex.ru>
2022-04-11 11:23:55 +0300

    learn tabs into js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent(){
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(numberTab = 0){
+        tabsContent[numberTab].style.display = 'block';
+        tabs[numberTab].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')){
+            tabs.forEach((item, number) => {
+                if(target == item){
+                    hideTabContent();
+                    showTabContent(number);
+                }
+                console.log('click');
+            });
+        }
+    });
+
+});
